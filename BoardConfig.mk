c0e38f59ec8aecf9058e7dccc96c148ae29ab9b4
Evgenii Stepanov <eugenis@google.com>
2023-06-05 16:25:52 -0700

    topaz: Disable Kernel MTE
    Bug: 280343521
    Test: cat /proc/cmdline | grep kasan=off
    Change-Id: I42781e8efdc4cf5a89baca1af8c3d62a4984c4f2

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -101,7 +101,8 @@ BOARD_BOOTCONFIG := \
     androidboot.usbcontroller=4e00000.dwc3
 
 BOARD_KERNEL_CMDLINE := \
-    disable_dma32=on
+    kasan=off \
+    disable_dma32=on \
     rcu_nocbs=all \
     rcutree.enable_rcu_lazy=1
 

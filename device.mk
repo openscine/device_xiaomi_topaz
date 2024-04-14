f19503047e566836ceea06bdb41ea96d94de24a1
bengris32 <bengris32@protonmail.ch>
2024-04-14 16:04:01 +0300

    topaz: Build OSS `vendor.xiaomi.hardware.fingerprintextension@1.0` interface
    Change-Id: I9f245a32bdab1f024c038291f983fa75b5dc8a29
    Signed-off-by: artemscine <artempemine@gmail.com>

--- device.mk
+++ device.mk
@@ -236,6 +236,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
+PRODUCT_PACKAGES += \
+    vendor.xiaomi.hardware.fingerprintextension@1.0.vendor
+
 # Gatekeeper
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0.vendor

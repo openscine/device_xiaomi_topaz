69bf44b74a83d8b4939df99172bfd462d821c69d
Alexander Winkowski <dereference23@outlook.com>
2024-04-18 22:18:17 +0300

    topaz: Build Xiaomi common fingerprint service * Also remove proprietary android.hardware.biometrics.fingerprint@2.1-service_lc
    Change-Id: I8775527c7951ddc9e16955da18d155dede6fe192
    Signed-off-by: artemscine <artempemine@gmail.com>

--- device.mk
+++ device.mk
@@ -237,6 +237,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
 PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.3-service.xiaomi \
     vendor.xiaomi.hardware.fingerprintextension@1.0.vendor \
     android.hardware.biometrics.fingerprint-V2-ndk.vendor \
     android.hardware.biometrics.common-V2-ndk.vendor \

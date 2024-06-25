af4f4f51462b16183ad287310d6c2f131f94293e
Michael Bestas <mikeioannina@gmail.com>
2024-06-25 20:33:56 +0300

    topaz: Build missing libraries for 14 QPR3
    Change-Id: I6a84526ca1e9f4e5140c8068646433cab3fe85b2
    Signed-off-by: artemscine <artempemine@gmail.com>

--- device.mk
+++ device.mk
@@ -137,6 +137,7 @@ PRODUCT_PACKAGES += \
     libdng_sdk.vendor \
     libgui_vendor \
     libstdc++_vendor \
+    libutilscallstack.vendor \
     vendor.qti.hardware.camera.device@1.0.vendor \
     vendor.qti.hardware.camera.postproc@1.0.vendor
 
@@ -400,7 +401,8 @@ PRODUCT_PACKAGES += \
 
 # Net
 PRODUCT_PACKAGES += \
-    android.system.net.netd@1.1.vendor
+    android.system.net.netd@1.1.vendor \
+    libnetutils.vendor
 
 # NFC
 PRODUCT_PACKAGES += \
@@ -466,12 +468,21 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 
+# QCC
+PRODUCT_PACKAGES += \
+    libgrpc++_unsecure.vendor
+
 # QMI
 PRODUCT_PACKAGES += \
+    libcurl.vendor \
     libjson \
+    libjsoncpp.vendor \
+    libqti_vndfwk_detect \
     libqti_vndfwk_detect.vendor \
     libqti_vndfwk_detect_vendor \
-    libvndfwk_detect_jni.qti.vendor
+    libsqlite.vendor \
+    libvndfwk_detect_jni.qti \
+    libvndfwk_detect_jni.qti.vendor 
 
 # QTI service tracker
 PRODUCT_PACKAGES += \

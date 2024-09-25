39ea58659735a92e15cff67c006a814a2face55c
Michael Bestas <mkbestas@lineageos.org>
2024-09-26 01:39:32 +0300

    topaz: Switch to AIDL thermal HAL
    Change-Id: I6ae621194044ab66fad69b9ef997b3b347bf6120
    Signed-off-by: artemscine <artempemine@gmail.com>

--- device.mk
+++ device.mk
@@ -530,7 +530,7 @@ PRODUCT_COPY_FILES += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@2.0-service.qti-v2 \
+    android.hardware.thermal-service.qti \
     libpsi.vendor
 
 # Trusted User Interface

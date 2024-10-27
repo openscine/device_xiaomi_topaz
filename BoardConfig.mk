8562d518156a6c042cfe1fa30012cdb8b5e1128c
Ali Hasan <ahb7671@gmail.com>
2024-10-27 18:32:58 +0500

    topaz: fix build failure due to non-existent file
    * FileExistsError: [Errno 17] File exists: 'out/target/product/tapas/vendor_dlkm/lib/modules/modules.blocklist' -> 'out/target/product/tapas/tmp9219be9k/lib/modules/modules.blocklist'
    * And add missing system_dlm modules.load
    
    Change-Id: Iebd1f4c3a223c46dba2d22e22558f5f5e94e3124
    Signed-off-by: Ali Hasan <ahb7671@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -118,8 +118,8 @@ TARGET_PREBUILT_KERNEL := $(KERNEL_PATH)/kernel
 PRODUCT_COPY_FILES += $(TARGET_PREBUILT_KERNEL):kernel
 
 # Kernel modules
+BOARD_SYSTEM_KERNEL_MODULES_LOAD := $(strip $(shell cat $(KERNEL_PATH)/system_dlkm/modules.load))
 BOARD_VENDOR_KERNEL_MODULES_LOAD := $(strip $(shell cat $(KERNEL_PATH)/vendor_dlkm/modules.load))
-BOARD_VENDOR_KERNEL_MODULES_BLOCKLIST_FILE :=  $(KERNEL_PATH)/vendor_dlkm/modules.blocklist
 BOARD_VENDOR_RAMDISK_KERNEL_MODULES_LOAD := $(strip $(shell cat  $(KERNEL_PATH)/vendor_ramdisk/modules.load))
 BOARD_VENDOR_RAMDISK_KERNEL_MODULES_BLOCKLIST_FILE := $(KERNEL_PATH)/vendor_ramdisk/modules.blocklist
 BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD := $(strip $(shell cat $(KERNEL_PATH)/vendor_ramdisk/modules.load.recovery))
